73d07f1be1fd73989140e2685bd2ff97f94f891a
Mateusz Kmieć <64418079+Haivex@users.noreply.github.com>
2021-06-15 16:48:21 +0200

    feat: dark and light theme (#4)
    Co-authored-by: Haivex <demozial@gmail.com>

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { darkTheme, lightTheme } from './constants/Theme';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -16,7 +17,7 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <PaperProvider>
+        <PaperProvider theme={colorScheme === 'light' ? lightTheme : darkTheme}>
           <Navigation colorScheme={colorScheme} />
           <StatusBar />
         </PaperProvider>
