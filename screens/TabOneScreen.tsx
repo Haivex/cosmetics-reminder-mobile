90a7eaefc9f1bf60252d9b466af6899c772bee5d
Mateusz Kmieć <64418079+Haivex@users.noreply.github.com>
2021-06-15 15:50:48 +0200

    feat: time picker (#2)
    Co-authored-by: Haivex <demozial@gmail.com>

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import DatePickerInput from '../components/DatePickerInput';
-
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import TimePickerInput from '../components/TimePickerInput';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Utwórz zadanie</Text>
       <DatePickerInput />
+      <TimePickerInput/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
   },
   title: {
