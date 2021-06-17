6a2b9e8e202fa5d4254e06671a0a164e3f830ff4
Haivex <demozial@gmail.com>
2021-06-17 16:40:19 +0200

    feat: create task form

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
+import { Button, TextInput, Text, HelperText } from 'react-native-paper';
 import DatePickerInput from '../components/DatePickerInput';
-import { Text, View } from '../components/Themed';
+import { View } from '../components/Themed';
 import TimePickerInput from '../components/TimePickerInput';
 import Constants from 'expo-constants';
 import { useForm, Controller } from 'react-hook-form';
@@ -10,7 +10,7 @@ import { useForm, Controller } from 'react-hook-form';
 type Time = {
   hours: number;
   minutes: number;
-}
+};
 
 type FormData = {
   date: Date;
@@ -19,29 +19,68 @@ type FormData = {
 };
 
 export default function TabOneScreen() {
-
-  const { control, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
   const onSubmit = (data: FormData) => console.log(data);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tytuł: </Text>
-      <Controller<FormData> control={control} rules={{
-         required: true,
-        }} render={({field: {onChange, onBlur, value }}) => (
-          <TextInput onBlur={onBlur}
-          onChangeText={value => onChange(value)}
-          value={value as string} mode='outlined' placeholder='Tytuł zadania' />)} name='title'
-        />
+      <Controller<FormData>
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            value={value as string}
+            mode='outlined'
+            placeholder='Tytuł zadania'
+          />
+        )}
+        name='title'
+      />
+      <HelperText type='error' visible={errors.title ? true : false}>
+        Uzupełnij tytuł
+      </HelperText>
+
       <Text style={styles.title}>Data rozpoczęcia: </Text>
-      <Controller<FormData> control={control} rules={{
-         required: true,
-        }} render={({field: {onBlur, onChange}}) => (<DatePickerInput onBlur={onBlur} onChange={onChange} />)} name='date' />
+      <Controller<FormData>
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onBlur, onChange } }) => (
+          <DatePickerInput onBlur={onBlur} onChange={onChange} />
+        )}
+        name='date'
+      />
+      <HelperText type='error' visible={errors.date ? true : false}>
+        Uzupełnij datę
+      </HelperText>
+
       <Text style={styles.title}>Godzina rozpoczęcia: </Text>
-      <Controller<FormData> control={control} rules={{
-         required: true,
-        }} render={({field: {onBlur, onChange }}) => (<TimePickerInput onBlur={onBlur} onChange={onChange} />)} name='time' />
-      <Button onPress={(handleSubmit(onSubmit))} mode='outlined'>Ustaw</Button>
+      <Controller<FormData>
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onBlur, onChange } }) => (
+          <TimePickerInput onBlur={onBlur} onChange={onChange} />
+        )}
+        name='time'
+      />
+      <HelperText type='error' visible={errors.time ? true : false}>
+        Uzupełnij czas
+      </HelperText>
+      <Button onPress={handleSubmit(onSubmit)} mode='outlined'>
+        Ustaw
+      </Button>
     </View>
   );
 }
