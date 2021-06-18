26fc05dcd6eb60f0217016d87b6121baf111462c
Mateusz Kmieć <64418079+Haivex@users.noreply.github.com>
2021-06-18 14:44:12 +0200

    chore: adding todo to store (#6)
    Co-authored-by: Haivex <demozial@gmail.com>

---
+++ store/MainStore.tsx
@@ -0,0 +1,62 @@
+import { TaskData, Time } from '../screens/TabOneScreen';
+
+type Task = {
+  index: number;
+  title: string;
+  time: Time;
+  date: Date;
+  completed: boolean;
+};
+
+type AppState = {
+  todos: Task[];
+};
+
+export const initialState: AppState = {
+  todos: [
+    {
+      index: 0,
+      title: 'Learn React',
+      time: { hours: 12, minutes: 50 },
+      date: new Date(),
+      completed: true,
+    },
+    {
+      index: 1,
+      title: 'Learn Redux',
+      time: { hours: 13, minutes: 50 },
+      date: new Date(),
+      completed: false,
+    },
+    {
+      index: 2,
+      title: 'Build something fun!',
+      time: { hours: 14, minutes: 50 },
+      date: new Date(),
+      completed: false,
+    },
+  ],
+};
+
+type Action = { type: 'ADD_TODO'; payload: TaskData };
+
+export default function appReducer(state: AppState, action: Action): AppState {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            index: state.todos[state.todos.length - 1].index + 1,
+            title: action.payload.title,
+            date: action.payload.date,
+            time: action.payload.time,
+            completed: false,
+          },
+        ],
+      };
+    default:
+      throw new Error();
+  }
+}
