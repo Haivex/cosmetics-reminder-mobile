86b6a5c7013e024427f74978568769c091b88fb1
Mateusz Kmieć <64418079+Haivex@users.noreply.github.com>
2021-06-25 17:07:11 +0200

    Translation (#16)
    * chore: add translation config
    
    * chore: add translations
    
    * chore: translate two screens
    
    * chore: screen title and bottom tab translation
    
    * chore: task menu without input
    
    * chore: rename input translation
    
    * chore: date and time label translation
    
    * chore: default locale
    
    * time and date input translation
    
    * chore: translate relative date
    
    Co-authored-by: Haivex <demozial@gmail.com>

--- components/DatePickerInput.tsx
+++ components/DatePickerInput.tsx
@@ -1,13 +1,21 @@
 import 'intl';
 import 'intl/locale-data/jsonp/pl';
+import 'intl/locale-data/jsonp/en';
 import * as React from 'react';
 import { TextInput } from 'react-native-paper';
 import { DatePickerModal } from 'react-native-paper-dates';
 import { format } from 'date-fns';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import i18n from 'i18n-js';
 
 interface DatePickerInputProps {
   onBlur: () => void; onChange: (...event: any[]) => void;
+}
+
+function getDatePattern() {
+  const currentLocale = i18n.currentLocale();
+  if(currentLocale === 'en-US') return 'LL-dd-uu'
+  return 'dd-LL-uu'
 }
 
 export default function DatePickerInput({onBlur, onChange}: DatePickerInputProps) {
@@ -27,21 +35,20 @@ export default function DatePickerInput({onBlur, onChange}: DatePickerInputProps
     },
     [setOpen, setDate]
   );
-
   return (
     <>
-      <TextInput right={<TextInput.Icon name='calendar' />} onTouchEnd={() => setOpen(true)} mode='outlined' placeholder='Wybierz datę'>
-        {date && format(date, 'dd-LL-uu')}
+      <TextInput right={<TextInput.Icon name='calendar' />} onTouchEnd={() => setOpen(true)} mode='outlined' placeholder={i18n.t('datePicker.label')}>
+        {date ? format(date, getDatePattern()) : ''} 
       </TextInput>
       <DatePickerModal
-        locale='pl'
+        locale={i18n.currentLocale()}
         mode='single'
         visible={open}
         onDismiss={onDismissSingle}
         date={date}
         onConfirm={onConfirmSingle}
-        saveLabel='Potwierdź'
-        label='Wybierz datę'
+        saveLabel={i18n.t('datePicker.acceptButton')}
+        label={i18n.t('datePicker.label')}
       />
     </>
   );
