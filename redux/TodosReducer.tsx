6097df6ec03c403cbb45e158799dd1de3945977e
Haivex <demozial@gmail.com>
2021-06-28 14:17:59 +0200

    chore: store task with uuid

--- redux/TodosReducer.tsx
+++ redux/TodosReducer.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { TaskData, Time } from '../screens/TabOneScreen';
+import { SavedTask, Time } from '../screens/TabOneScreen';
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { CyclicInterval } from '../components/CyclicTaskInputs';
+import { getNotifications } from '../notificationsStorage/asyncStorage';
 
 export type Task = {
-  index: number;
+  id: string;
   title: string;
   time: Time;
   date: Date;
@@ -24,7 +25,7 @@ export type AppState = {
 export const globalState: AppState = {
   todos: [
     {
-      index: 0,
+      id: '0',
       title: 'Learn React',
       time: { hours: 12, minutes: 50 },
       date: new Date(),
@@ -32,7 +33,7 @@ export const globalState: AppState = {
       cyclicInterval: undefined,
     },
     {
-      index: 1,
+      id: '1',
       title: 'Learn Redux',
       time: { hours: 13, minutes: 50 },
       date: new Date(2022, 7, 20),
@@ -40,7 +41,7 @@ export const globalState: AppState = {
       cyclicInterval: undefined,
     },
     {
-      index: 2,
+      id: '2',
       title: 'Build something fun!',
       time: { hours: 14, minutes: 50 },
       date: new Date(2021, 5, 13),
@@ -54,9 +55,9 @@ const todosSlice = createSlice({
   name: "todos",
   initialState: globalState,
   reducers: {
-    addTodo(state, action: PayloadAction<TaskData>) {
+    addTodo(state, action: PayloadAction<SavedTask>) {
       state.todos = [...state.todos, {
-        index: state.todos[state.todos.length - 1].index + 1,
+        id: action.payload.id,
         title: action.payload.title,
         date: new Date(action.payload.date as Date),
         time: action.payload.time,
@@ -65,19 +66,19 @@ const todosSlice = createSlice({
     }]
     },
     markTodoCompleted(state, action: PayloadAction<Task>) {
-      const index = state.todos.findIndex(todo => todo.index === action.payload.index);
+      const index = state.todos.findIndex(todo => todo.id === action.payload.id);
       if (index !== -1) state.todos[index].completed = true;
     },
     restoreTodo(state, action: PayloadAction<Task>) {
-      const index = state.todos.findIndex(todo => todo.index === action.payload.index);
+      const index = state.todos.findIndex(todo => todo.id === action.payload.id);
       if (index !== -1) state.todos[index].completed = false;
     },
     renameTodo(state, action: PayloadAction<RenameTaskPayload>) {
-      const index = state.todos.findIndex(todo => todo.index === action.payload.task.index);
+      const index = state.todos.findIndex(todo => todo.id === action.payload.task.id);
       if (index !== -1) state.todos[index].title = action.payload.title;
     },
     deleteTodo(state, action: PayloadAction<Task>) {
-      state.todos = state.todos.filter((todo) => todo.index !== 2)
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload.id)
     },
   }
 })
