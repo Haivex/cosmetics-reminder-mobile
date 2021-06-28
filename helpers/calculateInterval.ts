4e9a86012cb2f71d85a9b924e2994abfb46539ad
Haivex <demozial@gmail.com>
2021-06-28 14:07:10 +0200

    chore: helper function

---
+++ helpers/calculateInterval.ts
@@ -0,0 +1,11 @@
+import { CyclicInterval } from '../components/CyclicTaskInputs';
+import {SECONDS_IN_DAY, SECONDS_IN_HOUR, SECONDS_IN_MINUTE} from '../constants/TimeValues';
+
+export default function convertCyclicIntervalToSeconds(cyclicInterval: CyclicInterval): number {
+    const days = cyclicInterval.days || 0;
+    const hours = cyclicInterval.hours || 0;
+    const minutes = cyclicInterval.minutes || 0;
+
+    const seconds = (days * SECONDS_IN_DAY) + (hours + SECONDS_IN_HOUR) + (minutes * SECONDS_IN_MINUTE);
+    return seconds; 
+}
