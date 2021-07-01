1df6073b0f0e80bd074bc261d6c412a056e3fa3e
Haivex <demozial@gmail.com>
2021-07-01 15:24:20 +0200

    chore: authentication screen

---
+++ authentication/Auhentication.tsx
@@ -0,0 +1,78 @@
+// https://docs.expo.io/guides/authentication/#google
+
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { Button, Platform } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+import { ChildrenProp } from '../components/NotificationWrapper';
+
+WebBrowser.maybeCompleteAuthSession();
+
+export default function Authentication({ children }: ChildrenProp) {
+  if (!process.env.EXPO_CLIENT_ID) {
+    throw new Error('No EXPO_CLIENT_ID env');
+  }
+  const [isLogged, setLogged] = React.useState(false);
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: process.env.EXPO_CLIENT_ID,
+    // iosClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+    // androidClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+    // webClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+  });
+
+  React.useEffect(() => {
+    const checkIfLogged = async () => {
+      if (!process.env.EXPO_AUTH_STATE_KEY) {
+        throw new Error('No EXPO_AUTH_STATE_KEY env');
+      }
+
+      const secretAuthKey = await SecureStore.getItemAsync(
+        process.env.EXPO_AUTH_STATE_KEY
+      );
+      if (secretAuthKey) {
+        setLogged(true);
+      }
+    };
+
+    checkIfLogged()
+    WebBrowser.warmUpAsync();
+
+    return () => {
+      WebBrowser.coolDownAsync();
+    };
+  }, []);
+
+  React.useEffect(() => {
+    if (response && response.type === 'success') {
+      const { authentication } = response;
+      const storageValue = JSON.stringify(authentication);
+
+      if (!process.env.EXPO_AUTH_STATE_KEY) {
+        throw new Error('No EXPO_AUTH_STATE_KEY env');
+      }
+
+      if (Platform.OS !== 'web') {
+        SecureStore.setItemAsync(process.env.EXPO_AUTH_STATE_KEY, storageValue);
+      }
+
+      setLogged(true);
+    }
+  }, [response]);
+
+  return (
+    <>
+      {isLogged ? (
+        children
+      ) : (
+        <Button
+          disabled={!request}
+          title='Login'
+          onPress={() => {
+            promptAsync();
+          }}
+        />
+      )}
+    </>
+  );
+}
