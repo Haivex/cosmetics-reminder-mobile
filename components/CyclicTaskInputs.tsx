3269d303aa79f61e4d0e67a9d3fb9e40b7ad2901
Mateusz Kmieć <64418079+Haivex@users.noreply.github.com>
2021-07-01 12:18:12 +0200

    Cyclic Tasks (#17)
    * chore: cyclic checkbox
    
    * chore: clear form on submit
    
    * chore: cyclic task inputs
    
    * chore: translation cyclic inputs
    
    * chore: show inputs if checkbox checked
    
    * chore: clear interval inputs
    
    * chore: save interval in redux
    
    * test: only cylic task are catched
    
    * chore: helper function
    
    * chore: async storage
    
    * chore: time constants
    
    * chore: generated task id
    
    * chore: store task with uuid
    
    * chore: async storage methods
    
    * chore: set cyclic notifications
    
    * chore: delete cyclic tasks
    
    * fix: missing key
    
    * fix: seconds calculation and possible rejection
    
    * chore: random values lib
    
    * fix: render current and future tasks
    
    * fix: getting notifiation data
    
    * fix: cancel notification
    
    * fix: saved todo to notfication
    
    * fix: typo
    
    * chore: work in progress
    
    * fix: cancelling notification
    
    Co-authored-by: Haivex <demozial@gmail.com>

---
+++ components/CyclicTaskInputs.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import i18n from 'i18n-js';
+import { TextInput } from 'react-native-paper';
+import { View, Text } from 'react-native';
+
+interface CyclicDataInputProps {
+  onBlur: () => void;
+  onChange: (...event: any[]) => void;
+  value: CyclicInterval;
+}
+
+export type CyclicInterval = {
+  days: number;
+  hours: number;
+  minutes: number;
+};
+
+const CyclicTaskInputs = ({
+  onChange,
+  onBlur,
+  value
+}: CyclicDataInputProps) => {
+  return (
+    <View>
+      <Text>{i18n.t('createTaskScreen.cyclicInputs.days')}</Text>
+      <TextInput
+        keyboardType='numeric'
+        onBlur={onBlur}
+        onChangeText={(valueFromInputs: string) => {
+          onChange({ ...value, days: Number(valueFromInputs) });
+        }}
+        value={value?.days?.toString() || '0'}
+        mode='outlined'
+      />
+      <Text>{i18n.t('createTaskScreen.cyclicInputs.hours')}</Text>
+      <TextInput
+        keyboardType='numeric'
+        onBlur={onBlur}
+        onChangeText={(valueFromInput: string) => {
+          onChange({ ...value, hours: Number(valueFromInput) });
+        }}
+        value={value?.hours?.toString() || '0'}
+        mode='outlined'
+      />
+      <Text>{i18n.t('createTaskScreen.cyclicInputs.minutes')}</Text>
+      <TextInput
+        keyboardType='numeric'
+        onBlur={onBlur}
+        onChangeText={(valueFromInput: string) => {
+          onChange({ ...value, minutes: Number(valueFromInput) });
+        }}
+        value={value?.minutes?.toString() || '0'}
+        mode='outlined'
+      />
+    </View>
+  );
+};
+
+export default CyclicTaskInputs;
