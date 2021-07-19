419a5dc4c3e3529067016b1cff344f5d3bd6a061
Haivex <demozial@gmail.com>
2021-07-19 16:39:44 +0200

    chore: apple authentication

--- authentication/FacebookDevelopmentAuthentication.tsx
+++ authentication/FacebookDevelopmentAuthentication.tsx
@@ -1,4 +1,4 @@
-import       * as React from 'react';
+import * as React from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import * as Facebook from 'expo-auth-session/providers/facebook';
 import { ResponseType } from 'expo-auth-session';
