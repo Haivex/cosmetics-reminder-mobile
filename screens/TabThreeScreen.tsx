d94e004070ead59e62ec4987a56e5e08d97d4661
Mateusz Kmieć <64418079+Haivex@users.noreply.github.com>
2021-07-19 14:47:08 +0200

    Small improvements (#30)
    * chore: remove unused dependencies
    
    * chore: autofocus first input
    
    * chore: caret hidden in date and time picker
    
    * chore: don't show keyboard in time and date picker
    
    * chore: autofocus date picker
    
    * chore: autofocus timeref
    
    * chore: autofocus if empty
    
    * chore: clear and return button key
    
    * chore: change default screen
    
    * chore: navigate to second screen on submit
    
    * chore: add google services json
    
    * chore: ignore google-services json file
    
    * chore: add version code
    
    * chore: google android auth
    
    * chore: tasks sort
    
    * chore: sort fiished tasks
    
    * chore: refresh screen every minute
    
    * fix: task without repeating if not cyclic
    
    Co-authored-by: Haivex <demozial@gmail.com>

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -3,12 +3,23 @@ import { ScrollView } from 'react-native';
 import { RootState } from '../redux/MainStore';
 import { useSelector } from 'react-redux';
 import { DoneTask } from '../components/DoneTask';
+import { set } from 'date-fns';
 
 export default function TabThreeScreen() {
     const { todos } = useSelector((state: RootState) => state.todos);
 
     const getDoneTasks = () => {
-        return todos.filter((task) => task.completed)
+        return todos.filter((task) => task.completed).sort((previousTask, currentTask) => {
+            const previousTaskDateWithTime = set(previousTask.date, {
+              hours: previousTask.time.hours,
+              minutes: previousTask.time.minutes
+            })
+            const currentTaskDateWithTime = set(currentTask.date, {
+              hours: currentTask.time.hours,
+              minutes: currentTask.time.minutes
+            })
+            return currentTaskDateWithTime.getTime() - previousTaskDateWithTime.getTime()
+          })
       }
 
     return (
