3ec437847f7ef44c8091a935609fc74b411540f6
Haivex <demozial@gmail.com>
2021-07-21 13:52:39 +0200

    chore: apple auth button

--- authentication/AppleAuthentication.tsx
+++ authentication/AppleAuthentication.tsx
@@ -1,21 +1,38 @@
-import * as AppleAuthentication from 'expo-apple-authentication';
+import * as React from 'react';
+import * as ExpoAppleAuthentication from 'expo-apple-authentication';
+import * as SecureStore from 'expo-secure-store';
 
-function YourComponent() {
+export default function AppleAuthentication() {
   return (
-    <AppleAuthentication.AppleAuthenticationButton
-      buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
-      buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
+    <ExpoAppleAuthentication.AppleAuthenticationButton
+      buttonType={ExpoAppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
+      buttonStyle={ExpoAppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
       cornerRadius={5}
       style={{ width: 200, height: 44 }}
       onPress={async () => {
         try {
-          const credential = await AppleAuthentication.signInAsync({
+          const credential = await ExpoAppleAuthentication.signInAsync({
             requestedScopes: [
-              AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-              AppleAuthentication.AppleAuthenticationScope.EMAIL,
+              ExpoAppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+              ExpoAppleAuthentication.AppleAuthenticationScope.EMAIL,
             ],
           });
           // signed in
+          //console.log('Successful logged: ', credential.user);
+
+          if (process.env.EXPO_AUTH_STATE_KEY === undefined) {
+            throw new Error('No EXPO_AUTH_STATE_KEY env');
+          }
+  
+          const storageValue = JSON.stringify({
+              authProvider: 'APPLE',
+              authData: {
+                  credential,
+              }
+          })
+  
+          SecureStore.setItemAsync(process.env.EXPO_AUTH_STATE_KEY, storageValue);
+
         } catch (e) {
           if (e.code === 'ERR_CANCELED') {
             // handle that the user canceled the sign-in flow
