c1db9840f7df5322d2ed3cc9c9a723bb9b13be2b
Mateusz Kmieć <64418079+Haivex@users.noreply.github.com>
2021-07-27 16:06:48 +0200

    Authentication buttons (#33)
    * chore: facebook authentication
    
    * chore: facebook developent authentication
    
    * chore: google development authentication
    
    * chore: apple authentication
    
    * chore: facebook login buttons
    
    * chore: apple auth button
    
    * chore: google auth buttons
    
    * chore: auth buttons
    
    * chore: login reducer
    
    * chore: redux login state
    
    * fix: store provider wrap authentication
    
    * chore: center login button
    
    Co-authored-by: Haivex <demozial@gmail.com>

---
+++ authentication/Authentication.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import * as SecureStore from 'expo-secure-store';
+import { ChildrenProp } from '../components/NotificationWrapper';
+import AuthButtons from './AuthButtons';
+import { useDispatch, useSelector } from 'react-redux';
+import { logIn } from '../redux/LoginReducer';
+import { RootState } from '../redux/MainStore';
+
+export default function Authentication({ children }: ChildrenProp) {
+  const dispatch = useDispatch();
+  const loginInfo = useSelector((state: RootState) => state.login)
+
+  const checkIfLogged = async () => {
+    if (!process.env.EXPO_AUTH_STATE_KEY) {
+      throw new Error('No EXPO_AUTH_STATE_KEY env');
+    }
+
+    const secretAuthKey = await SecureStore.getItemAsync(
+      process.env.EXPO_AUTH_STATE_KEY
+    );
+    if (secretAuthKey) {
+      dispatch(logIn(JSON.parse(secretAuthKey)))
+    }
+  };
+
+  React.useEffect(() => {
+    checkIfLogged();
+  }, [])
+  
+  return <>{loginInfo.isLogged ? children : <AuthButtons />}</>;
+}
