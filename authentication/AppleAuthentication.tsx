dba9421669e5882631b3601a6f4603bb273a8632
Haivex <demozial@gmail.com>
2021-08-01 17:57:22 +0200

    chore: apple auth firebase

--- authentication/AppleAuthentication.tsx
+++ authentication/AppleAuthentication.tsx
@@ -3,6 +3,8 @@ import * as ExpoAppleAuthentication from 'expo-apple-authentication';
 import * as SecureStore from 'expo-secure-store';
 import { useDispatch } from 'react-redux';
 import { UserInfo, logIn } from '../redux/LoginReducer';
+import firebase from 'firebase';
+import { registration } from '../database/registration';
 
 export default function AppleAuthentication() {
   const dispatch = useDispatch();
@@ -13,41 +15,88 @@ export default function AppleAuthentication() {
       cornerRadius={5}
       style={{ width: 200, height: 44 }}
       onPress={async () => {
-        try {
-          const credential = await ExpoAppleAuthentication.signInAsync({
-            requestedScopes: [
-              ExpoAppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-              ExpoAppleAuthentication.AppleAuthenticationScope.EMAIL,
-            ],
+        let provider = new firebase.auth.OAuthProvider('apple.com');
+
+        provider.addScope('email');
+        provider.addScope('name');
+
+        provider.setCustomParameters({
+          // Localize the Apple authentication screen in French.
+          locale: 'en',
+        });
+
+        firebase
+          .auth()
+          .signInWithPopup(provider)
+          .then((result) => {
+            /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+
+            // The signed-in user info.
+            var user = result.user;
+
+            if (process.env.EXPO_AUTH_STATE_KEY === undefined) {
+              throw new Error('No EXPO_AUTH_STATE_KEY env');
+            }
+
+            const loginInfo: UserInfo = {
+              authProvider: 'APPLE',
+              authData: {
+                credential,
+                user,
+              },
+            };
+
+              const storageValue = JSON.stringify(loginInfo);
+
+              SecureStore.setItemAsync(
+                process.env.EXPO_AUTH_STATE_KEY,
+                storageValue
+              );
+              dispatch(logIn(loginInfo));
+
+            if (/*!account*/ null) {
+              registration(loginInfo);
+            } else {
+              firebase.auth().signInWithRedirect(provider);
+            }
           });
-          // signed in
-          //console.log('Successful logged: ', credential.user);
 
-          if (process.env.EXPO_AUTH_STATE_KEY === undefined) {
-            throw new Error('No EXPO_AUTH_STATE_KEY env');
-          }
+        // try {
+        //   const credential = await ExpoAppleAuthentication.signInAsync({
+        //     requestedScopes: [
+        //       ExpoAppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+        //       ExpoAppleAuthentication.AppleAuthenticationScope.EMAIL,
+        //     ],
+        //   });
+        //   // signed in
+        //   //console.log('Successful logged: ', credential.user);
 
-          const loginInfo: UserInfo = {
-            authProvider: 'APPLE',
-            authData: {
-              credential,
-            },
-          };
+        //   if (process.env.EXPO_AUTH_STATE_KEY === undefined) {
+        //     throw new Error('No EXPO_AUTH_STATE_KEY env');
+        //   }
 
-          const storageValue = JSON.stringify(loginInfo);
+        //   const loginInfo: UserInfo = {
+        //     authProvider: 'APPLE',
+        //     authData: {
+        //       credential,
+        //     },
+        //   };
 
-          SecureStore.setItemAsync(
-            process.env.EXPO_AUTH_STATE_KEY,
-            storageValue
-          );
-          dispatch(logIn(loginInfo));
-        } catch (e) {
-          if (e.code === 'ERR_CANCELED') {
-            // handle that the user canceled the sign-in flow
-          } else {
-            // handle other errors
-          }
-        }
+        //   const storageValue = JSON.stringify(loginInfo);
+
+        //   SecureStore.setItemAsync(
+        //     process.env.EXPO_AUTH_STATE_KEY,
+        //     storageValue
+        //   );
+        //   dispatch(logIn(loginInfo));
+        // } catch (e) {
+        //   if (e.code === 'ERR_CANCELED') {
+        //     // handle that the user canceled the sign-in flow
+        //   } else {
+        //     // handle other errors
+        //   }
+        // }
       }}
     />
   );
