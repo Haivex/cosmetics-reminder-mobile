03459b4df0689b8f4ff739bd4efc878efdc2d76f
Haivex <demozial@gmail.com>
2021-08-02 15:27:04 +0200

    chore: does user exist

--- firebase/doesUserExist.ts
+++ firebase/doesUserExist.ts
@@ -1,9 +1,9 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
 
-const doesUserExist = async () => {
+const doesUserExist = async (givenEmail: string) => {
     let db = firebase.firestore();
-    const querySnapshot = await db.collection('users').where('email', '==', 'given@email.com').get();
+    const querySnapshot = await db.collection('users').where('email', '==', givenEmail).get();
     return !querySnapshot.empty;
 }
 
