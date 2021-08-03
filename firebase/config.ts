ef96f3080b12641c3d660b02dd5e463a91dd39b8
Haivex <demozial@gmail.com>
2021-08-03 17:25:03 +0200

    fix: firebase config

--- firebase/config.ts
+++ firebase/config.ts
@@ -6,6 +6,7 @@ export default {
       projectId: process.env.FIREBASE_PROJECT_ID,
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
     }
 }
