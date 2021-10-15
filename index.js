dc17ebe01109eb7b9bdd40c26b2749ad0cf7c65e
Haivex <demozial@gmail.com>
2021-10-15 18:25:21 +0200

    ignore eventemitter errors

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-get-random-values';
+import {LogBox} from 'react-native';
+
+/* eslint-disable quotes */
+
+LogBox.ignoreLogs([
+  `EventEmitter.removeListener('change', ...): Method has been deprecated.`,
+]);
 
 AppRegistry.registerComponent(appName, () => App);
