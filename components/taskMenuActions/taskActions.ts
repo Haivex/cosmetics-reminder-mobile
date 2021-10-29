4f9a9c0a339db32efc161b2e489de9c559f75469
Haivex <demozial@gmail.com>
2021-10-29 19:02:26 +0200

    navigate to rename dialog action

--- components/taskMenuActions/taskActions.ts
+++ components/taskMenuActions/taskActions.ts
@@ -8,45 +8,51 @@ initTranslation();
 
 export const renameAction: SingleAction = {
   title: translate('taskMenu.changeTitle'),
-  callback: task => {
-    //TODO: navigate to rename task screen
-    console.log('Rename task' + task);
+  callback: (task, appState) => {
+    appState?.navigation.current?.navigate('RenameTaskDialog', task);
   },
 };
 
 export const completeAction: SingleAction = {
   title: translate('taskMenu.finishTask'),
   callback: task => {
-    updateTaskCompletion(task.id, true);
+    updateTaskCompletion(task.id, true).catch(error => {
+      console.error('Complete task error: ', error);
+    });
   },
 };
 
 export const deleteAction: SingleAction = {
   title: translate('taskMenu.deleteTask'),
   callback: (task, appState) => {
-    deleteTask(task.id).then(() => {
-      const storedNotification =
-        appState?.globalState.notifications.storedNotifications.find(
-          notification => notification.taskId === task.id,
+    deleteTask(task.id)
+      .then(() => {
+        const storedNotification =
+          appState?.globalState.notifications.storedNotifications.find(
+            notification => notification.taskId === task.id,
+          );
+        Notifications.cancelLocalNotification(
+          storedNotification?.notificationId.toString() || '',
         );
-      Notifications.cancelLocalNotification(
-        storedNotification?.notificationId.toString() || '',
-      );
-    });
+      })
+      .catch(error => {
+        console.error('Delete task error: ', error);
+      });
   },
 };
 
 export const restoreAction: SingleAction = {
   title: translate('taskMenu.restoreTask'),
   callback: task => {
-    updateTaskCompletion(task.id, false);
+    updateTaskCompletion(task.id, false).catch(error => {
+      console.error('Update task error: ', error);
+    });
   },
 };
 
 export const editAction: SingleAction = {
   title: translate('taskMenu.editTask'),
   callback: (task, appState) => {
-    console.log('Task edition' + task);
     appState?.navigation.current?.navigate('TaskEdition', task);
   },
 };
