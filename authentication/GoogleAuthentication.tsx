4cc2dedb40dde4ce3a66979f4e35a76fe5e948bd
Haivex <demozial@gmail.com>
2021-11-05 14:56:48 +0100

    Auth loading indicators

--- authentication/GoogleAuthentication.tsx
+++ authentication/GoogleAuthentication.tsx
@@ -3,24 +3,14 @@ import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import React, {useEffect} from 'react';
 import {GoogleSocialButton} from 'react-native-social-buttons';
 import ErrorDialog from '../components/ErrorDialog';
+import {AuthButtonProps} from './Authentication';
 
 GoogleSignin.configure({
   webClientId:
     '231511460067-mdt6l95v5id047d7dhngmo8lprmelpah.apps.googleusercontent.com',
 });
 
-async function onGoogleButtonPress() {
-  // Get the users ID token
-  const {idToken} = await GoogleSignin.signIn();
-
-  // Create a Google credential with the token
-  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-  // Sign-in the user with the credential
-  return auth().signInWithCredential(googleCredential);
-}
-
-function GoogleSignInButton() {
+function GoogleSignInButton({disabled, setLoading}: AuthButtonProps) {
   const [error, setError] = React.useState('');
 
   useEffect(() => {
@@ -28,15 +18,32 @@ function GoogleSignInButton() {
       isLoggedIn ? GoogleSignin.signOut() : null,
     );
   }, []);
+
+  async function onGoogleButtonPress() {
+    setLoading(true);
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
+
   return (
     <>
       <GoogleSocialButton
+        disabled={disabled}
         onPress={() =>
           onGoogleButtonPress()
             .then(() => console.info('Signed in with Google!'))
             .catch(catchedError => {
               console.error('Google Sign-in Error:', catchedError);
               setError(catchedError);
+            })
+            .finally(() => {
+              setLoading(false);
             })
         }
       />
