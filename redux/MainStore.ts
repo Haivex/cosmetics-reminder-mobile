dee50024723d3709fd9ceed058c5f3d46ca6f2fc
Haivex <demozial@gmail.com>
2021-11-07 15:47:08 +0100

    add firestore and firebase to blacklist

--- redux/MainStore.ts
+++ redux/MainStore.ts
@@ -27,6 +27,7 @@ const persistConfig = {
   version: 1,
   storage: reduxStorage,
   stateReconciler: autoMergeLevel2,
+  blacklist: ['firebase', 'firestore'],
 };
 
 const persistedReducer = persistReducer(
