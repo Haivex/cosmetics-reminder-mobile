e780c4c22123aa40cd81a250f606a4ab9aafafc1
Haivex <demozial@gmail.com>
2021-11-08 16:20:12 +0100

    no task card style

--- components/NoTasksCard.tsx
+++ components/NoTasksCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {StyleSheet, View} from 'react-native';
 import {Card, Text} from 'react-native-paper';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {translate} from '../translation/config';
@@ -9,13 +10,34 @@ interface NoTaskProps {
 
 const NoTasksCard = ({additionalText}: NoTaskProps) => {
   return (
-    <Card>
-      <Card.Content>
-        <MaterialIcon name="card-bulleted-off" />
-        <Text>{translate('noTask.defaultText')}</Text>
-        {additionalText && <Text>{additionalText}</Text>}
+    <Card style={styles.card}>
+      <Card.Content style={styles.content}>
+        <MaterialIcon size={50} name="card-bulleted-off-outline" />
+        <View>
+          <Text style={styles.text}>{translate('noTask.defaultText')}</Text>
+          {additionalText && <Text style={styles.text}>{additionalText}</Text>}
+        </View>
       </Card.Content>
     </Card>
   );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    aspectRatio: 3,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
+
 export default NoTasksCard;
