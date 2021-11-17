cdcf2bc8515bd8c697f6f9f7d8c9eb5eba6bfba4
Mateusz Kmieć <64418079+Haivex@users.noreply.github.com>
2021-11-17 18:23:22 +0100

    Notifications switch (#76)
    * no logs
    
    * don't check permissions
    
    * dev button to console notifications
    
    * cancel notifs on turn off and restore on turn on
    
    * remove permission request and abandon
    
    * schedule task
    
    * dev notifications buttons
    
    * allow notification while idle
    
    * remove unused code
    
    * refactor save task
    
    Co-authored-by: Haivex <demozial@gmail.com>

--- components/NotificationsStatus.tsx
+++ components/NotificationsStatus.tsx
@@ -1,18 +1,70 @@
 import React from 'react';
 import {Switch} from 'react-native-paper';
 import {useDispatch} from 'react-redux';
-import {toggleNotificationsStatus} from '../redux/NotificationsReducer';
+import {
+  addNotification,
+  clearNotifications,
+  togglePermission,
+} from '../redux/NotificationsReducer';
 import {useTrackedSelector} from '../redux/RootReducer';
-import {selectNotificationsStatus} from '../redux/selectors';
+import {
+  selectCurrentUser,
+  selectNotificationsStatus,
+  selectTasks,
+} from '../redux/selectors';
+import Notifications from 'react-native-push-notification';
+import {useFirestoreConnect} from 'react-redux-firebase';
+import {convertCyclicIntervalToSeconds} from '../helpers/intervalHelpers';
 
 const NotificationsStatus = () => {
+  const currentDate = new Date();
   const state = useTrackedSelector();
   const dispatch = useDispatch();
 
+  const user = selectCurrentUser(state);
   const notificationsStatus = selectNotificationsStatus(state);
+  useFirestoreConnect([
+    {
+      collection: 'tasks',
+      where: [
+        ['userUID', '==', user?.uid],
+        ['completed', '==', false],
+        ['date', '>', currentDate],
+      ],
+      orderBy: ['date', 'asc'],
+      storeAs: 'incomingTasks',
+    },
+  ]);
+  const {incomingTasks} = selectTasks(state);
 
-  const onToggleSwitch = () => {
-    dispatch(toggleNotificationsStatus(notificationsStatus));
+  const onToggleSwitch = (newValue: boolean) => {
+    if (!newValue) {
+      Notifications.cancelAllLocalNotifications();
+      dispatch(clearNotifications());
+    } else {
+      incomingTasks.forEach(task => {
+        const notificationCreationTimestamp = Date.now();
+        console.log(task);
+        Notifications.localNotificationSchedule({
+          channelId: 'main',
+          id: notificationCreationTimestamp,
+          title: 'Only You',
+          message: task.title,
+          date: new Date(task.date.toDate()),
+          allowWhileIdle: true,
+          repeatType: task.cyclicInterval ? 'time' : undefined,
+          repeatTime: task.cyclicInterval
+            ? convertCyclicIntervalToSeconds(task.cyclicInterval) * 1000
+            : 1,
+        });
+        const notificationToStore = {
+          taskId: task.id,
+          notificationId: notificationCreationTimestamp,
+        };
+        dispatch(addNotification(notificationToStore));
+      });
+    }
+    dispatch(togglePermission(newValue));
   };
 
   return <Switch value={notificationsStatus} onValueChange={onToggleSwitch} />;
