48eeb361115501ae3cb45cfee5f008e8674ae548
Haivex <demozial@gmail.com>
2021-11-18 18:17:12 +0100

    replace creation code with notifications singleton

--- screens/TaskCreationScreen.tsx
+++ screens/TaskCreationScreen.tsx
@@ -2,18 +2,15 @@ import {set} from 'date-fns';
 import React from 'react';
 import 'react-native-get-random-values';
 import {CalendarDate} from 'react-native-paper-dates/lib/typescript/src/Date/Calendar';
-import Notifications from 'react-native-push-notification';
-import {useDispatch} from 'react-redux';
 import {CyclicInterval} from '../components/taskForm/inputs/CyclicTaskInputs';
 import TaskForm from '../components/taskForm/TaskForm';
 import {Time} from '../components/taskForm/inputs/TimePickerInput';
 import {TaskDocument} from '../firebase/types';
 import {saveTask} from '../firebase/saveTask';
-import {convertCyclicIntervalToSeconds} from '../helpers/intervalHelpers';
-import {addNotification} from '../redux/NotificationsReducer';
 import '../translation/config';
 import {translate} from '../translation/config';
 import {NavigationProps} from '../components/navigation/types';
+import TaskNotifications from '../shared/TaskNotifications';
 export type TaskData = {
   date: CalendarDate;
   time: Time;
@@ -25,7 +22,6 @@ export default function TaskCreationScreen({
   route,
   navigation,
 }: NavigationProps) {
-  const dispatch = useDispatch();
   const onSubmit = (data: TaskData) => {
     data.cyclicInterval = data.cyclicInterval || undefined;
     const mergedDateAndTime = set(data.date as Date, data.time);
@@ -34,27 +30,16 @@ export default function TaskCreationScreen({
       date: mergedDateAndTime,
       time: undefined,
     };
-    return saveTask(taskDataWithoutTime).then(savedTask => {
-      const id = savedTask.id;
-      const dataFromDb = savedTask.data() as TaskDocument;
-      const notificationCreationTimestamp = Date.now();
-      Notifications.localNotificationSchedule({
-        channelId: 'main',
-        id: notificationCreationTimestamp,
-        title: 'Only You',
-        message: dataFromDb.title,
-        date: dataFromDb.date.toDate(),
-        allowWhileIdle: true,
-        repeatType: dataFromDb.cyclicInterval ? 'time' : undefined,
-        repeatTime: dataFromDb.cyclicInterval
-          ? convertCyclicIntervalToSeconds(dataFromDb.cyclicInterval) * 1000
-          : 1,
-      });
-      const notificationToStore = {
-        taskId: id,
-        notificationId: notificationCreationTimestamp,
+    return saveTask(taskDataWithoutTime).then(taskDocumentFromDatabase => {
+      const id = taskDocumentFromDatabase.id;
+      const taskDataFromDatabase =
+        taskDocumentFromDatabase.data() as TaskDocument;
+      const task = {
+        id,
+        ...taskDataFromDatabase,
       };
-      dispatch(addNotification(notificationToStore));
+
+      TaskNotifications.createNotification(task);
     });
   };
 
