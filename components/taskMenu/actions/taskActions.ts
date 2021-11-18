55964f66ede1fc01b0fd1595861716fead4b3131
Haivex <demozial@gmail.com>
2021-11-18 18:24:51 +0100

    cancel notifications in task actions

--- components/taskMenu/actions/taskActions.ts
+++ components/taskMenu/actions/taskActions.ts
@@ -1,6 +1,6 @@
-import Notifications from 'react-native-push-notification';
 import {deleteTask} from '../../../firebase/deleteTask';
 import {updateTaskCompletion} from '../../../firebase/updateTaskCompletion';
+import TaskNotifications from '../../../shared/TaskNotifications';
 import initTranslation, {translate} from '../../../translation/config';
 import {SingleAction} from '../TaskMenu';
 
@@ -16,24 +16,20 @@ export const renameAction: SingleAction = {
 export const completeAction: SingleAction = {
   title: translate('taskMenu.finishTask'),
   callback: task => {
-    updateTaskCompletion(task.id, true).catch(error => {
-      console.error('Complete task error: ', error);
-    });
+    updateTaskCompletion(task.id, true)
+      .then(() => TaskNotifications.cancelNotification(task))
+      .catch(error => {
+        console.error('Complete task error: ', error);
+      });
   },
 };
 
 export const deleteAction: SingleAction = {
   title: translate('taskMenu.deleteTask'),
-  callback: (task, appState) => {
+  callback: task => {
     deleteTask(task.id)
       .then(() => {
-        const storedNotification =
-          appState?.globalState.notifications.storedNotifications.find(
-            notification => notification.taskId === task.id,
-          );
-        Notifications.cancelLocalNotification(
-          storedNotification?.notificationId.toString() || '',
-        );
+        TaskNotifications.cancelNotification(task);
       })
       .catch(error => {
         console.error('Delete task error: ', error);
