1506b80680894a43d1b44ecb7508875ecc4a007a
Haivex <demozial@gmail.com>
2021-11-22 17:03:04 +0100

    focus bar while visible and blur while invisible

--- components/search/SearchBar.tsx
+++ components/search/SearchBar.tsx
@@ -10,9 +10,18 @@ type SearchBarTopProps = {
 
 const SearchBarTop = ({visible, onChangeText}: SearchBarTopProps) => {
   const [searchQuery, setSearchQuery] = React.useState('');
+  const searchBarRef = React.useRef<Searchbar>();
   const styles = StyleSheet.create({
     searchbar: {display: visible ? 'flex' : 'none'},
   });
+
+  React.useEffect(() => {
+    if (visible) {
+      searchBarRef.current?.focus();
+    } else {
+      searchBarRef.current?.blur();
+    }
+  }, [visible]);
 
   const onChangeSearch = (query: string) => {
     setSearchQuery(query);
@@ -21,10 +30,13 @@ const SearchBarTop = ({visible, onChangeText}: SearchBarTopProps) => {
 
   return (
     <Searchbar
+      focusable={visible}
+      ref={searchBarRef}
       style={styles.searchbar}
       placeholder={translate('search')}
       onChangeText={onChangeSearch}
       value={searchQuery}
+      autoFocus={visible}
     />
   );
 };
