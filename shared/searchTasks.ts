9a48b0076884d2f4ee7865653afb37a19714e5d5
Mateusz Kmieć <64418079+Haivex@users.noreply.github.com>
2021-11-25 17:12:10 +0100

    Search bar (#83)
    * no logs
    
    * Show/hide search bar and call fn on text change
    
    * focus bar while visible and blur while invisible
    
    * search by date, title
    
    * search task by cyclic property
    
    * clear search bar on hide
    
    * lower case searching
    
    * fix: do not render search bar if invisible
    
    * wrap function into useCallback
    
    * search done tasks
    
    * fix: task are now removed from list while search
    
    * return immediately if query is empty
    
    Co-authored-by: Haivex <demozial@gmail.com>

---
+++ shared/searchTasks.ts
@@ -0,0 +1,56 @@
+import {Task} from '../types';
+
+const searchTasks = (tasks: Task[], query: string): Task[] => {
+  if (!query) {
+    return tasks;
+  }
+
+  let filteredTasks = [...tasks];
+
+  const filters = ['before:', 'after:', 'cyclic:'];
+  let foundedFilter: string | undefined = '';
+  let isFilterTurnedOn = false;
+
+  const searchedTitleWords: string[] = [];
+  const queriesWords = query.split(' '); //space
+
+  queriesWords.forEach(word => {
+    if (!isFilterTurnedOn) {
+      foundedFilter = filters.find(filter => filter === word);
+      if (foundedFilter) {
+        isFilterTurnedOn = true;
+        return;
+      }
+    }
+    if (isFilterTurnedOn) {
+      switch (foundedFilter) {
+        case 'before:':
+          filteredTasks = filteredTasks.filter(
+            task => task.date.toDate() < new Date(word),
+          );
+          break;
+        case 'after:':
+          filteredTasks = filteredTasks.filter(
+            task => task.date.toDate() > new Date(word),
+          );
+          break;
+        case 'cyclic:':
+          filteredTasks = filteredTasks.filter(task => {
+            if (word === 'true') {
+              return typeof task.cyclicInterval !== 'undefined';
+            }
+            return typeof task.cyclicInterval === 'undefined';
+          });
+          break;
+      }
+      isFilterTurnedOn = false;
+      return;
+    }
+    searchedTitleWords.push(word);
+  });
+  const searchedTitle = searchedTitleWords.join(' ').toLowerCase(); //space
+  return filteredTasks.filter(task =>
+    task.title.toLowerCase().includes(searchedTitle),
+  );
+};
+export default searchTasks;
