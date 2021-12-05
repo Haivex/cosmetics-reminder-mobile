25c2a55aa657708129d18d4614fd69d17987147b
Haivex <demozial@gmail.com>
2021-12-05 16:44:53 +0100

    fix: node modules compatible with react native

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /**
  * @format
  */
-require('node-libs-react-native/globals');
-import {AppRegistry, LogBox} from 'react-native';
+//require('node-libs-react-native/globals');
+import { AppRegistry, LogBox } from 'react-native';
 import 'react-native-gesture-handler';
 import 'react-native-get-random-values';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 /* eslint-disable quotes */
 
--- shared/Logger.ts
+++ shared/Logger.ts
@@ -1,37 +1,37 @@
-import {format} from 'date-fns';
-import Colors from 'colors.ts';
+// import {format} from 'date-fns';
+// import Colors from 'colors.ts';
 
-Colors.enable();
+// Colors.enable();
 
-export default class Logger {
-  static shouldShowInfo = true;
-  static shouldShowWarn = true;
-  static shouldShowError = true;
+// export default class Logger {
+//   static shouldShowInfo = true;
+//   static shouldShowWarn = true;
+//   static shouldShowError = true;
 
-  static info(message: string, data?: unknown) {
-    if (!this.shouldShowInfo) {
-      return;
-    }
+//   static info(message: string, data?: unknown) {
+//     if (!this.shouldShowInfo) {
+//       return;
+//     }
 
-    Colors.theme({info: ['cyan', 'bgBlack']});
+//     Colors.theme({info: ['cyan', 'bgBlack']});
 
-    const date = format(new Date(), 'yyyy-MM-dd, HH:mm:ss');
-    const formattedInfoMessage = `%c[${date}] INFO: ${message} `.info;
+//     const date = format(new Date(), 'yyyy-MM-dd, HH:mm:ss');
+//     const formattedInfoMessage = `%c[${date}] INFO: ${message} `.info;
 
-    // const formattedInfoMessage = `%c[${date}] INFO: ${message} `;
-    // const messageStyle = [
-    //   'color: cyan',
-    //   'background-color: #333',
-    //   'padding: 2px 4px',
-    //   'border-radius: 2px',
-    // ].join(';');
+//     // const formattedInfoMessage = `%c[${date}] INFO: ${message} `;
+//     // const messageStyle = [
+//     //   'color: cyan',
+//     //   'background-color: #333',
+//     //   'padding: 2px 4px',
+//     //   'border-radius: 2px',
+//     // ].join(';');
 
-    if (data) {
-      //return console.info(formattedInfoMessage, messageStyle, data);
-      return console.info(formattedInfoMessage, data);
-    }
+//     if (data) {
+//       //return console.info(formattedInfoMessage, messageStyle, data);
+//       return console.info(formattedInfoMessage, data);
+//     }
 
-    //console.info(formattedInfoMessage, messageStyle);
-    console.info(formattedInfoMessage);
-  }
-}
+//     //console.info(formattedInfoMessage, messageStyle);
+//     console.info(formattedInfoMessage);
+//   }
+// }
