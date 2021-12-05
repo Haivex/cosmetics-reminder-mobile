a2d206653cc142f37ef15333115a72e22878b5ac
Haivex <demozial@gmail.com>
2021-12-05 17:18:38 +0100

    console log cyan info messages

--- shared/Logger.ts
+++ shared/Logger.ts
@@ -1,37 +1,30 @@
-// import {format} from 'date-fns';
-// import Colors from 'colors.ts';
+import {format} from 'date-fns';
 
-// Colors.enable();
+const cyanColor = '\x1b[36m';
+const resetAttributes = '\x1b[0m';
 
-// export default class Logger {
-//   static shouldShowInfo = true;
-//   static shouldShowWarn = true;
-//   static shouldShowError = true;
+export default class Logger {
+  static shouldShowInfo = true;
+  static shouldShowWarn = true;
+  static shouldShowError = true;
 
-//   static info(message: string, data?: unknown) {
-//     if (!this.shouldShowInfo) {
-//       return;
-//     }
+  static info(message: string, data?: unknown) {
+    if (!this.shouldShowInfo) {
+      return;
+    }
 
-//     Colors.theme({info: ['cyan', 'bgBlack']});
+    const date = format(new Date(), 'yyyy-MM-dd, HH:mm:ss');
 
-//     const date = format(new Date(), 'yyyy-MM-dd, HH:mm:ss');
-//     const formattedInfoMessage = `%c[${date}] INFO: ${message} `.info;
+    const formattedInfoMessage = `[${date}] INFO: ${message} `;
 
-//     // const formattedInfoMessage = `%c[${date}] INFO: ${message} `;
-//     // const messageStyle = [
-//     //   'color: cyan',
-//     //   'background-color: #333',
-//     //   'padding: 2px 4px',
-//     //   'border-radius: 2px',
-//     // ].join(';');
+    if (data) {
+      return console.info(
+        `${cyanColor}%s${resetAttributes}`,
+        formattedInfoMessage,
+        data,
+      );
+    }
 
-//     if (data) {
-//       //return console.info(formattedInfoMessage, messageStyle, data);
-//       return console.info(formattedInfoMessage, data);
-//     }
-
-//     //console.info(formattedInfoMessage, messageStyle);
-//     console.info(formattedInfoMessage);
-//   }
-// }
+    console.info(`${cyanColor}%s${resetAttributes}`, formattedInfoMessage);
+  }
+}
