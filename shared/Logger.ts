df8df79dffe6984c91aba081c58152dc9650f954
Haivex <demozial@gmail.com>
2021-12-08 15:45:00 +0100

    logger warn and error, refactor

--- shared/Logger.ts
+++ shared/Logger.ts
@@ -1,30 +1,117 @@
 import {format} from 'date-fns';
 
-const cyanColor = '\x1b[36m';
-const resetAttributes = '\x1b[0m';
+export enum MESSAGE_TYPE {
+  INFO = 'INFO',
+  WARN = 'WARN',
+  ERROR = 'ERROR',
+}
+
+enum TERMINAL_COLOR {
+  CYAN = '\x1b[36m',
+  YELLOW = '\x1b[33m',
+  RED = '\x1b[31m',
+}
+
+const RESET_ATTRIBUTE = '\x1b[0m';
 
 export default class Logger {
-  static shouldShowInfo = true;
-  static shouldShowWarn = true;
-  static shouldShowError = true;
+  private static shouldShowInfo = true;
+  private static shouldShowWarn = true;
 
   static info(message: string, data?: unknown) {
     if (!this.shouldShowInfo) {
       return;
     }
 
+    const formattedMessage = this.prepareMessageToLog(
+      MESSAGE_TYPE.INFO,
+      message,
+    );
+
+    this.logColoredMessage(
+      TERMINAL_COLOR.CYAN,
+      MESSAGE_TYPE.INFO,
+      formattedMessage,
+      data,
+    );
+  }
+
+  static warn(message: string, data?: unknown) {
+    if (!this.shouldShowWarn) {
+      return;
+    }
+
+    const formattedMessage = this.prepareMessageToLog(
+      MESSAGE_TYPE.WARN,
+      message,
+    );
+
+    this.logColoredMessage(
+      TERMINAL_COLOR.YELLOW,
+      MESSAGE_TYPE.WARN,
+      formattedMessage,
+      data,
+    );
+  }
+
+  static error(message: string, data?: unknown, error?: unknown) {
+    if (!this.shouldShowWarn) {
+      return;
+    }
+
+    const formattedMessage = this.prepareMessageToLog(
+      MESSAGE_TYPE.ERROR,
+      message,
+    );
+
+    this.logColoredMessage(
+      TERMINAL_COLOR.RED,
+      MESSAGE_TYPE.ERROR,
+      formattedMessage,
+      data,
+      error,
+    );
+  }
+
+  private static prepareMessageToLog(
+    messageType: MESSAGE_TYPE,
+    message: string,
+  ) {
     const date = format(new Date(), 'yyyy-MM-dd, HH:mm:ss');
 
-    const formattedInfoMessage = `[${date}] INFO: ${message} `;
+    const formattedMessage = `[${date}] ${messageType}: ${message} `;
+    return formattedMessage;
+  }
+
+  private static logColoredMessage(
+    color: TERMINAL_COLOR,
+    messageType: MESSAGE_TYPE,
+    message: string,
+    data?: unknown,
+    error?: unknown,
+  ) {
+    const consoleFunctionNameToInvoke = messageType.toLowerCase();
+
+    if (data && error) {
+      return console.error(
+        `${color}%s${RESET_ATTRIBUTE}`,
+        message,
+        data,
+        error,
+      );
+    }
 
     if (data) {
-      return console.info(
-        `${cyanColor}%s${resetAttributes}`,
-        formattedInfoMessage,
+      return console[consoleFunctionNameToInvoke](
+        `${color}%s${RESET_ATTRIBUTE}`,
+        message,
         data,
       );
     }
 
-    console.info(`${cyanColor}%s${resetAttributes}`, formattedInfoMessage);
+    console[consoleFunctionNameToInvoke](
+      `${color}%s${RESET_ATTRIBUTE}`,
+      message,
+    );
   }
 }
