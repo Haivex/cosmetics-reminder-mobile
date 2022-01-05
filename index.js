52faeb2a6ebb6c139faa7d7562407442b3508596
Haivex <demozial@gmail.com>
2022-01-05 14:54:10 +0100

    fix quotes problem

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-/**
- * @format
- */
-//require('node-libs-react-native/globals');
-import { AppRegistry, LogBox } from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import 'react-native-gesture-handler';
 import 'react-native-get-random-values';
 import App from './App';
-import { name as appName } from './app.json';
-
-/* eslint-disable quotes */
+import {name as appName} from './app.json';
 
 LogBox.ignoreLogs([
-  `EventEmitter.removeListener('change', ...): Method has been deprecated.`,
-  `NativeFirebaseError: [firestore/unknown] Cannot call useEmulator() after instance has already been initialized.`,
+  "EventEmitter.removeListener('change', ...): Method has been deprecated.",
+  'NativeFirebaseError: [firestore/unknown] Cannot call useEmulator() after instance has already been initialized.',
 ]);
 AppRegistry.registerComponent(appName, () => App);
