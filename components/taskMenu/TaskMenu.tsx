907655c678a9f22470722aa67a622145c6e77111
Haivex <demozial@gmail.com>
2022-01-17 16:58:10 +0100

    add icons to  task actions

--- components/taskMenu/TaskMenu.tsx
+++ components/taskMenu/TaskMenu.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import {IconButton, Menu} from 'react-native-paper';
 import {Task} from '../../types';
 import TaskMenuItem from './TaskMenuItem';
+import {IconSource} from 'react-native-paper/lib/typescript/components/Icon';
 
 export type SingleAction = {
   title: string;
   callback: (task: Task) => unknown;
+  icon?: IconSource;
 };
 
 type TaskMenuProps = {task: Task; actions: SingleAction[]};
@@ -26,6 +28,7 @@ const TaskMenu = ({actions, task}: TaskMenuProps) => {
           closeMenu={closeMenu}
           callback={singleAction.callback}
           title={singleAction.title}
+          icon={singleAction.icon}
         />
       ))}
     </Menu>
